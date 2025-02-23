ed628bb34ba268ed49c4cc44a61425c44a379c6c
Raj Khatri <khatriraj030@gmail.com>
2025-02-23 15:51:27 +0530

    incorrect import fix in onboarding page

--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import OnboardingForm from "./_components/OnboardingForm";
-import { industries } from "@/data/industries";
 import { getUserOnboardingStatus } from "@/actions/user";
 import { redirect } from "next/navigation";
 
